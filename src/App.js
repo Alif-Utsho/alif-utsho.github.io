b66532bc3642ec39e6d389a117e4d1e72bd30840
Alif-Utsho <ashahriar29@gmail.com>
2021-02-09 22:38:41 +0600

    deploy

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import './App.css';
 
 import Submit from './components/submit-page/submit'
 import CrushPage from './components/view-page/crush-page'
-import { Router } from '@reach/router'
+import { Redirect, Router } from '@reach/router'
 import Footer from './components/footer'
 
 const App = () => {
   return (
     <div>
         <Router>
-          <Submit path="propose/" />
-        <CrushPage path="propose/:name" />
+        <Submit path="propose/" />
+        <Redirect
+          from="propose/"
+          to="/"
+        />
+          <CrushPage path="propose/:name" />
         
         </Router>
         <Footer/>
     </div>
   );
 }
+
+// https://alif-utsho.github.io/propose
 
 export default App;
